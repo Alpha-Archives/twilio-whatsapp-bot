11e9ec390367279611110cb7be7e429c4bc0b763
Alpha Olomi <alphaolomi@gmail.com>
2019-08-10 15:00:34 +0300

    catch errors

--- controllers/api/chat.js
+++ controllers/api/chat.js
@@ -5,9 +5,7 @@ const MessagingResponse = require('twilio').twiml.MessagingResponse;
 async function reply(req, res, next) {
   const response = new MessagingResponse();
   const msg = response.message();
-  res.writeHead(200, { 'Content-Type': 'text/xml' });
   try {
-
     if (req.body.Body == 'hello') {
       msg.body('Hi!');
     } else if (req.body.Body == 'bye') {
@@ -15,12 +13,12 @@ async function reply(req, res, next) {
     } else {
       msg.body('No Body param match,  in the request to your server.');
     }
+
+    res.set('Content-Type', 'text/xml');
     res.send(msg.toString())
   } catch (error) {
-
     msg.body('Error!');
     res.send(response.toString())
-    // next(error);
   }
 }
 
@@ -34,7 +32,7 @@ async function replyMedia(req, res, next) {
     message.body('Hello World! ' + req.body.toString());
     // message.media('https://farm8.staticflickr.com/7090/6941316406_80b4d6d50e_z_d.jpg');
 
-    res.writeHead(200, { 'Content-Type': 'text/xml' });
+    res.set('Content-Type', 'text/xml');
     res.send(response.toString())
   } catch (error) {
 
